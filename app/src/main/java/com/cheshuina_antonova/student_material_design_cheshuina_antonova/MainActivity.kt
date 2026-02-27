16280da52f8359cf969cbcdf9020daa39673c605
eriyknas <anastasia9cheshuina@yandex.ru>
2026-02-27 10:15:30 +0300

    Initial commit

---
+++ app/src/main/java/com/cheshuina_antonova/student_material_design_cheshuina_antonova/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.cheshuina_antonova.student_material_design_cheshuina_antonova
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cheshuina_antonova.student_material_design_cheshuina_antonova.ui.theme.Student_Material_Design_Cheshuina_AntonovaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Student_Material_Design_Cheshuina_AntonovaTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Student_Material_Design_Cheshuina_AntonovaTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/test/java/com/cheshuina_antonova/student_material_design_cheshuina_antonova/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.cheshuina_antonova.student_material_design_cheshuina_antonova
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
